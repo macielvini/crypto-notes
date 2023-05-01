f0d5ff8f420cb9d115b774e477d73e60285aec2e
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-01 16:30:38 -0300

    refactor: users password encryption

--- back-end/src/users/users.module.ts
+++ back-end/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersRepository } from './users.repository';
+import { EncryptModule } from 'src/encrypt/encrypt.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EncryptModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService, UsersRepository],
--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
+import { BcryptService } from 'src/encrypt/bcrypt.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepo: UsersRepository) {}
+  constructor(
+    private readonly usersRepo: UsersRepository,
+    private readonly encryptService: BcryptService,
+  ) {}
 
   async create(body: CreateUserDto) {
-    return await this.usersRepo.create(body);
+    return await this.usersRepo.create({
+      ...body,
+      password: this.encryptService.hash(body.password),
+    });
   }
 
   async findAll() {
